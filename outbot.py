267adbf8544611e2e5e2dcaab8916819d5bcf256
sypeer <sy.peeran@gmail.com>
2016-10-14 02:47:05 +0530

    dev5

--- outbot.py
+++ outbot.py
@@ -66,7 +66,7 @@ def send_message(recipient_id, message_text):
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
-#    log(r.text)
+    log(r.text)
 
 
 def log(message):  # Wrapper for logging to stdout on heroku
