42c9377b704571e11d60276837df136badd94810
sypeer <sy.peeran@gmail.com>
2016-10-14 02:53:49 +0530

    dev5

--- outbot.py
+++ outbot.py
@@ -29,7 +29,7 @@ def handle_messages():
                 if messaging_event.get("message"):
                     sender_id = messaging_event["sender"]["id"]
                     recipient_id = messaging_event["recipient"]["id"]
-                    message_text = messaging_event["message"]["text"]
+                    message_text = messaging_event["message"]["seq"]
 
                     send_message(sender_id, message_text)
                     kitten(sender_id) 
