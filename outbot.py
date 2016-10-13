9e2f75fd5f7e67f60ded36b1ab8fbe5dcdf9477f
sypeer <sy.peeran@gmail.com>
2016-10-14 01:06:52 +0530

    dev3

--- outbot.py
+++ outbot.py
@@ -97,7 +97,7 @@ def kitten(recipient_id):
             }
         
 
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=payload)
     if r.status_code != 200:
         log(r.status_code)
     log(r.text)
