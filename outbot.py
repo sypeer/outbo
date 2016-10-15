0018b80df74772bd57a8b2aab3cffdaf38511986
sypeer <sy.peeran@gmail.com>
2016-10-15 17:20:23 +0530

    dev6

--- outbot.py
+++ outbot.py
@@ -289,6 +289,7 @@ def womens(recipient_id):
                                             }
                                         ]
                             }
+                        ]
                         }
                     }
                 }
