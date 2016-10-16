84241ff12a8febac5b71ab1e1eacc910ad3779ef
sypeer <sy.peeran@gmail.com>
2016-10-16 17:39:23 +0530

    dev6

--- outbot.py
+++ outbot.py
@@ -31,7 +31,7 @@ def handle_messages():
                 if messaging_event.get("message"):
                     sender_id = messaging_event["sender"]["id"]
                     recipient_id = messaging_event["recipient"]["id"]
-                    if messaging_event['message']['text']:
+                    if messaging_event['message']['text'] is None:
                         message_text = messaging_event["message"]["text"]
                     else:
                         pass
