4f9eeb7810d87ec60394a761758a4136349d8d05
sypeer <sy.peeran@gmail.com>
2016-10-16 20:33:51 +0530

    dev6

--- outbot.py
+++ outbot.py
@@ -33,12 +33,14 @@ def handle_messages():
                     sender_id = messaging_event["sender"]["id"]
                     recipient_id = messaging_event["recipient"]["id"]
 
-                    while messaging_event['message']['attachments'] is True:
+                    if messaging_event['message']['attachments'] is True:
                         messaging_event = messaging_event['message']['attachments'][1]
                         send_message(sender_id, 'Attachment received')
-                    else:
+                    elif messaging_event['message'][0] is not 'attachments'::
                         messaging_event = messaging_event['message']['seq']
                         send_message(sender_id, messaging_event)
+                    else:
+                        pass
                     
 
                    # send_message(sender_id, message_text)        
