bae70ae62ce05cf8b0dfe899e322887f7ed9a484
sypeer <sy.peeran@gmail.com>
2016-10-16 20:15:27 +0530

    dev6

--- outbot.py
+++ outbot.py
@@ -36,11 +36,9 @@ def handle_messages():
                     if messaging_event['message']['attachments'] is True:
                         messaging_event = messaging_event['message']['attachments'][1]
                         send_message(sender_id, 'Attachment received')
-                    elif messaging_event['message']['text'] is True:
+                    else:
                         messaging_event = messaging_event['message']['text']
                         send_message(sender_id, messaging_event)
-                    else:
-                        pass
                     
 
                    # send_message(sender_id, message_text)        
